f64bc5fa255fe55015b9558caa56021bc195ee74
Honza Javorek <mail@honzajavorek.cz>
2025-06-20 00:39:22 +0200

    prepare new architecture

---
+++ p3news/scrapers/expats.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from pprint import pp
+from urllib.parse import urljoin
+from zoneinfo import ZoneInfo
+from crawlee.crawlers import BeautifulSoupCrawler, BeautifulSoupCrawlingContext
+
+
+async def main() -> None:
+    crawler = BeautifulSoupCrawler()
+
+    @crawler.router.default_handler
+    async def default_handler(context: BeautifulSoupCrawlingContext) -> None:
+        await context.enqueue_links(selector=".top.title h3 a", label="article")
+        await context.enqueue_links(selector=".content article h3 a", label="article")
+
+    @crawler.router.handler("article")
+    async def article_handler(context: BeautifulSoupCrawlingContext) -> None:
+        dt_text = context.soup.select_one(".about .created").text.strip()
+        dt = datetime.strptime(dt_text, "Published on %d.%m.%Y %H:%M:%S")
+        dt = dt.replace(tzinfo=ZoneInfo("Europe/Prague"))
+
+        await context.push_data({
+            "title": context.soup.select_one(".title h1").text.strip(),
+            "author": context.soup.select_one(".about .written-by a").text.strip(),
+            "lead": context.soup.select_one(".title h3").text.strip(),
+            "image_url": urljoin(context.request.url, context.soup.select_one(".featured-image img")["src"]),
+            "url": context.request.url,
+            "tags": [tag.text.strip() for tag in context.soup.select(".categories a")],
+            "published_at": dt.isoformat(),
+        })
+
+    await crawler.run(
+        [
+            "https://www.expats.cz/czech-news/tag/prague-3",
+            "https://www.expats.cz/czech-news/tag/zizkov",
+        ]
+    )
+    data = await crawler.get_data()
+    pp(data.items)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
---
+++ p3news/scrapers/novatrojka.py
@@ -0,0 +1,42 @@
+from datetime import UTC, datetime
+from pprint import pp
+from bs4 import BeautifulSoup
+from crawlee import Request
+from crawlee.crawlers import HttpCrawler, HttpCrawlingContext
+import feedparser
+
+
+async def main() -> None:
+    crawler = HttpCrawler()
+
+    @crawler.router.default_handler
+    async def default_handler(context: HttpCrawlingContext) -> None:
+        feed = feedparser.parse(context.http_response.read())
+        for entry in feed.entries:
+            content = entry.content[0]["value"]
+            content_soup = BeautifulSoup(content, "html.parser")
+            data = {
+                "title": str(entry.title),
+                "lead": content_soup.select_one("p").get_text(" ", strip=True),
+                "url": str(entry.link),
+                "tags": ["Nová Trojka", "rodina"],
+                "published_at": datetime(*entry.published_parsed[:6], tzinfo=UTC).isoformat(),
+            }
+            await context.add_requests([Request.from_url(str(entry.link), label="article", user_data=data)])
+
+    @crawler.router.handler("article")
+    async def article_handler(context: HttpCrawlingContext) -> None:
+        data = dict(context.request.user_data)
+        soup = BeautifulSoup(context.http_response.read(), "html.parser")
+        data["image_url"] = soup.select_one('meta[property="og:image"]')["content"]
+        await context.push_data(data)
+
+    await crawler.run(["https://www.nova-trojka.cz/index.php/feed/"])
+    data = await crawler.get_data()
+    pp(data.items)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
