130de37e6b3ea17a6e0c94c42790b0f3d3ed98f0
Honza Javorek <mail@honzajavorek.cz>
2025-06-20 14:10:16 +0200

    add munipolis

---
+++ p3news/scrapers/bezpecnost.py
@@ -0,0 +1,52 @@
+from datetime import date, timedelta
+import json
+from pprint import pp
+from bs4 import BeautifulSoup
+from crawlee import Request
+from crawlee.crawlers import HttpCrawler, HttpCrawlingContext
+
+
+async def main(date_from: date | None = None, date_to: date | None = None) -> None:
+    date_from = date_from or (date.today() - timedelta(days=30))
+    date_to = date_to or (date.today() + timedelta(days=5))
+
+    crawler = HttpCrawler()
+
+    @crawler.router.default_handler
+    async def default_handler(context: HttpCrawlingContext) -> None:
+        data = json.loads(context.http_response.read())
+        events = [
+            event
+            for event in data["events"]
+            if event.get("administrativeDistrict") == "Praha 3"
+        ]
+        for event in events:
+            lead_soup = BeautifulSoup(event["description"], "html.parser")
+            lead = lead_soup.get_text(" ", strip=True)
+            await context.push_data(
+                {
+                    "title": event["title"],
+                    "lead": lead,
+                    "url": f"https://bezpecnost.praha.eu/udalosti/{event['relativeUrl']}",
+                    "tags": [event["type"]],
+                    "published_at": event["publication"]["date"],
+                    "lang": "cs",
+                }
+            )
+
+    url = (
+        "https://bezpecnost.praha.eu/Intens.CrisisPortalInfrastructureApp/events"
+        f"?from={date_from.isoformat()}T00:00:00.000Z&to={date_to.isoformat()}T00:00:00.000Z"
+        "&groupType=OSKS_ACTUALITY&showHistory=true"
+    )
+    await crawler.run(
+        [Request.from_url(url, headers={"Accept": "application/json, text/plain, */*"})]
+    )
+    data = await crawler.get_data()
+    pp(data.items)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
---
+++ p3news/scrapers/munipolis.py
@@ -0,0 +1,88 @@
+from datetime import date, datetime, timedelta
+import json
+from pprint import pp
+import re
+from zoneinfo import ZoneInfo
+from bs4 import BeautifulSoup
+from crawlee import Request
+from crawlee.crawlers import HttpCrawler, HttpCrawlingContext
+
+
+async def main(date_from: date | None = None, date_to: date | None = None) -> None:
+    date_from = date_from or (date.today() - timedelta(days=30))
+    date_to = date_to or (date.today() + timedelta(days=5))
+
+    crawler = HttpCrawler()
+
+    @crawler.router.default_handler
+    async def default_handler(context: HttpCrawlingContext) -> None:
+        api_token = re.search(
+            r'"mrApiToken":"([^"]+)"', context.http_response.read().decode()
+        ).group(1)
+        csrf_token = re.search(
+            r'"csrfToken":"([^"]+)"', context.http_response.read().decode()
+        ).group(1)
+        headers = {
+            "Authorization": f"Bearer {api_token}",
+            "Accept": "application/json",
+            "X-Requested-With": "XMLHttpRequest",
+            "X-CSRF-TOKEN": csrf_token,
+            "X-HTTP-METHOD-OVERRIDE": "GET",
+            "pagination": "cursor",
+            "Origin": "https://praha3.munipolis.cz",
+            "Referer": "https://praha3.munipolis.cz/",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:139.0) Gecko/20100101 Firefox/139.0",
+        }
+        request = Request.from_url(
+            "https://api.munipolis.com/api/timeline",
+            method="POST",
+            headers=headers,
+            payload=json.dumps(
+                {
+                    "filter": {
+                        "types": ["news", "calendarEvent"],
+                        "cityId": [3209],
+                    },
+                    "include": ["mrCity", "files", "poll", "images", "lastComment"],
+                    "order": ["isPinned", "-publishAt"],
+                    "cursor": None,
+                    "perPage": 50,
+                    "includeExpiredPosts": False,
+                }
+            ),
+            label="api",
+        )
+        await context.add_requests([request])
+
+    @crawler.router.handler("api")
+    async def api_handler(context: HttpCrawlingContext) -> None:
+        data = json.loads(context.http_response.read())
+        for article in data["data"]:
+            dt = datetime.fromisoformat(article["publishAt"]).replace(tzinfo=ZoneInfo("Europe/Prague")).isoformat()
+            if lead := article["description"].strip():
+                lead = re.sub(r"^(vážení|milí)\s*sousedé\s*,\s*", "", lead, flags=re.I)
+                lead = lead.split("\n")[0].strip()
+                lead = lead[0].upper() + lead[1:]
+            else:
+                lead = None
+            await context.push_data(
+                {
+                    "title": article["title"],
+                    "lead": lead,
+                    "url": article["shareUrl"],
+                    "image_url": article["image"]["data"]["path"] if article["image"] else None,
+                    "tags": [],
+                    "published_at": dt,
+                    "lang": "cs",
+                }
+            )
+
+    await crawler.run(["https://praha3.munipolis.cz/"])
+    data = await crawler.get_data()
+    pp(data.items)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
